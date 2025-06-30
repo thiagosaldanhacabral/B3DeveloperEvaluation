4f603af283dc90f0d66ee1e808cac7904a3714c4
Thiago Saldanha Cabral <pis1078@supero.com.br>
2025-06-29 21:48:49 -0300

    Melhorias no projeto ui

--- ui/b3-developer-evaluation-ui/src/app/app.config.ts
+++ ui/b3-developer-evaluation-ui/src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(), provideClientHydration(withEventReplay()),
-    
+    provideZonelessChangeDetection(), 
+    provideClientHydration(withEventReplay()),
+    provideRouter(appRoutes)    
   ]
 };
